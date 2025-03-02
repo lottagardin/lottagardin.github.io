f96ea6b2d36e00eaa63d4d7d75c6dac8c0a73003
lottagardin <lotta.gardin@gmail.com>
2025-03-02 18:49:19 +0200

    sää API vaihdettu

--- JSON/saaHelsinki.js
+++ JSON/saaHelsinki.js
@@ -2,40 +2,19 @@ function helsinkiSaa(data) {
 
     // Alustaa muuttujat, jotka viedään tekstinä html-sivulle
     var kaupunki = "Kaupunki: Helsinki"; //Koska funktiot ovat eri tiedostoissa, kaupunkia ei tarvitse hakea json-objektista. 
-    var lampotila = "Lämpötila: " + data.current.temperature + " C";
-    var tuuli = "Tuulen nopeus: " + data.current.wind_speed + " m/s";
-    var kuvaus = "Sää: ";
-    var kuvaukset = data.current.weather_descriptions; //Alustaa sääkuvaukset -arrayn
-    var kuvat = data.current.weather_icons; //Alustaa sääikonit -arrayn
-    var kuva = "";
+    var lampotila = "Lämpötila: " + data.current.temp_c + " C";
+    var tuuli = "Tuulen nopeus: " + data.current.wind_kph + " km/h";
+    var kuva = "<img src='" + data.current.condition.icon + "' >"
 
-    //Tarkistaa, onko kuvauksia enemmän kuin yksi. Mikäli on, ne lisätään listaan allekkain. 
-    if (kuvaukset.length > 1) {
-        for (var i = 0; i < kuvaukset.length; i++) {
-            kuvaus = kuvaus + kuvaukset[i] + "<br>"
-        }
-    }
-    else {
-        kuvaus = kuvaus + kuvaukset[0];
-    }
 
-    //Tarkistaa, onko kuvia enemmän kuin yksi. Mikäli on, ne lisätään yhteen.
-    if (kuvat.length > 1) {
-        for (var i = 0; i < kuvat.length; i++) {
-            kuva = kuva + "<img src='" + kuvat[i] + "'>";
-        }
-    }
-    else {
-        kuva = kuva + "<img src='" + kuvat[0] + "' >";
-    }
 
     //Vie tiedot saa.html -tiedostolle.
-    document.getElementById("helsinkiSaa").innerHTML = kaupunki + "<br>" + kuvaus + "<br>" + lampotila + "<br>" + tuuli + "<br>" + kuva;
+    document.getElementById("helsinkiSaa").innerHTML = kaupunki + "<br>" + lampotila + "<br>" + tuuli + "<br>" + kuva;
 
 }
 
 //Hakee säätiedot
-fetch('https://api.weatherstack.com/current?access_key=11644898d7933ad3b678ce8516e01553&query=Helsinki')
+fetch('https://api.weatherapi.com/v1/current.json?key=feb2dc7e7a7b45dea9b163808250203&q=Helsinki&aqi=no')
 
 
     //Muokkaa vastauksen json-objektiksi
--- JSON/saaTampere.js
+++ JSON/saaTampere.js
@@ -2,39 +2,19 @@ function saa(data) {
 
     //Alustaa muuttujat
     var kaupunki = "Kaupunki: Tampere"; //Kaupunkia ei tarvitse hakea objektista, sillä eri kaupunkien hakufunktiot ovat eri tiedostoissa.
-    var lampotila = "Lämpötila: " + data.current.temperature + " C";
-    var tuuli = "Tuulen nopeus: " + data.current.wind_speed + " m/s";
-    var kuvaus = "Sää: ";
-    var kuvaukset = data.current.weather_descriptions; //Alustaa sääkuvaukset -arrayn
-    var kuvat = data.current.weather_icons; //Alustaa sääikonit -arrayn
-    var kuva = "";
+    var lampotila = "Lämpötila: " + data.current.temp_c + " C";
+    var tuuli = "Tuulen nopeus: " + data.current.wind_kph + " km/h";
+    var kuva = "<img src='" + data.current.condition.icon + "' >"
 
-    if (kuvaukset.length > 1) { //Tarkistaa, onko kuvauksia useampi kuin yksi. Jos on, ne lisätään listaan allekkain.
-        for (var i = 0; i < kuvaukset.length; i++) {
-            kuvaus = kuvaus + kuvaukset[i] + "<br>";
-        }
-    }
-    else {
-        kuvaus = kuvaus + kuvaukset[0];
-    }
-
-    if (kuvat.length > 1) { //Tarkistaa, onko sääikoneita useampi kuin yksi. Mikäli on, ne lisätään yhteen. 
-        for (var i = 0; i < kuvat.length; i++) {
-            kuva = kuva + "<img src='" + kuvat[i] + "'>";
-        }
-    }
-    else {
-        kuva = kuva + "<img src='" + kuvat[0] + "' >";
-    }
 
     //Vie tiedot saa.html -sivulle. 
-    document.getElementById("tampereSaa").innerHTML = kaupunki + "<br>" + kuvaus + "<br>" + lampotila + "<br>" + tuuli + "<br>" + kuva;
+    document.getElementById("tampereSaa").innerHTML = kaupunki + "<br>" + lampotila + "<br>" + tuuli + "<br>" + kuva;
 
 }
 
 
 //Hakee Tampereen säätiedot
-fetch('https://api.weatherstack.com/current?access_key=11644898d7933ad3b678ce8516e01553&query=Tampere')
+fetch('https://api.weatherapi.com/v1/current.json?key=feb2dc7e7a7b45dea9b163808250203&q=Tampere&aqi=no')
 
     //Muokkaa vastauksen json-objektiksi
     .then(function (response) {
