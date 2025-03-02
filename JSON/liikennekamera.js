b976d27c9a34d3b52b49cc20ed23ddd9ba237b3e
lottagardin <lotta.gardin@gmail.com>
2025-03-02 16:05:31 +0200

    liikennekamerasivu tehty

---
+++ JSON/liikennekamera.js
@@ -0,0 +1,41 @@
+function kuvat(data) {
+
+    var teksti = "";
+    var kuvat = data.properties.presets;
+    var aika = data.properties.dataUpdatedTime;
+
+    teksti = "<h1>Autolla tampereelle</h1><br><p>Minkälainen keli siellä on?</p>";
+
+    for (var i = 0; i < kuvat.length; i++) {
+
+        teksti = teksti + aika + "<br><img src='" + kuvat[i].imageUrl + "'>";
+
+    }
+
+    document.getElementById("vastaus").innerHTML = teksti;
+
+}
+
+
+fetch('https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507')
+
+
+    .then(function (response) {
+
+        return response.json();
+
+    })
+
+
+    .then(function (responseJson) {
+        console.log(responseJson);
+
+        kuvat(responseJson);
+
+    })
+
+    .catch(function (error) {
+
+        document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+
+    })
